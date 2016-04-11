aeaf56168b62839eb53efa7d9a1f97a947172f54
Adam <Adam@ADAM-PC>
2016-04-11 22:56:40 +0200

    Add project files.

---
+++ rysowanie/Profil.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rysowanie
+{
+    class Profil
+    {
+        private List<Warstwa> _warstwy;
+        private double _zwierciadloUstalone;
+        private double _zwierciadloNawiercone;
+        private double _glebokosc;
+
+        internal List<Warstwa> Warstwy
+        {
+            get
+            {
+                return _warstwy;
+            }
+
+            set
+            {
+                _warstwy = value;
+            }
+        }
+        public double ZwierciadloUstalone
+        {
+            get
+            {
+                return _zwierciadloUstalone;
+            }
+
+            set
+            {
+                _zwierciadloUstalone = value;
+            }
+        }
+        public double ZwierciadloNawiercone
+        {
+            get
+            {
+                return _zwierciadloNawiercone;
+            }
+
+            set
+            {
+                _zwierciadloNawiercone = value;
+            }
+        }
+
+        public double Glebokosc
+        {
+            get
+            {
+                return _glebokosc;
+            }
+        }
+
+        public Profil()
+        {
+
+        }
+
+        public void NowaWarstwa(Warstwa warstwa)
+        {
+            Warstwy.Add(warstwa);
+            foreach (Warstwa w in _warstwy)
+            {
+                _glebokosc += w.GlebokoscSpagu - w.GlebokoscStropu;
+            }
+        }
+        public void UsunWarstwe(int indeks)
+        {
+            _glebokosc -= _warstwy[indeks].GlebokoscSpagu - _warstwy[indeks].GlebokoscSpagu;
+            Warstwy.RemoveAt(indeks);
+            
+        }
+
+    }
+
+}
---
+++ rysowanie/Warstwa.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rysowanie
+{
+    class Warstwa : Profil
+    {
+        private string _nazwa;
+        private double _miazszosc;
+        private double _wspolczynnikFiltracji;
+        private double _glebokoscStropu;
+        private double _glebokoscSpagu;
+        private Brush _grafika;
+
+        public string Nazwa{  get { return _nazwa; }  }
+        public double Miazszosc { get { return _miazszosc; } }
+        public double WspolczynnikFiltracji { get { return _wspolczynnikFiltracji; } }
+        public double GlebokoscStropu { get { return _glebokoscStropu; } }
+        public double GlebokoscSpagu { get { return _glebokoscSpagu; } }
+        public Brush Grafika
+        {
+            get
+            {
+                return _grafika;
+            }
+
+            set
+            {
+                _grafika = value;
+            }
+        }
+
+        public Warstwa(Brush grafika, string nazwa, double miazszosc, double wspolczynnikFiltracji, double glebokoscStropu, double glebokoscSpagu)
+        {
+            _grafika = grafika;
+            _nazwa = nazwa;
+            _miazszosc= miazszosc;
+            _wspolczynnikFiltracji= wspolczynnikFiltracji;
+            _glebokoscStropu= glebokoscStropu;
+            _glebokoscSpagu= glebokoscSpagu;
+        }
+        
+    }
+}
